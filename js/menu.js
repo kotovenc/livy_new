ff0208ba73a8e0878f8dad37da9a15e0c58361d7
Leff <leosergkie@gmail.com>
2018-08-11 19:21:09 +0300

    Some fixes

--- js/menu.js
+++ js/menu.js
@@ -16,8 +16,19 @@ $(window).scroll(function () {
     $('.logobg').css('opacity', opacity);
 });
 
+/*после первого клика по бургеру 
+второй клик (в любом месте) закрывает меню*/
 let every_second_click = 0;
+var delay = 500;
 $('.menu-icon').click(function(){
+    var lastTime = +localStorage.lastTime;
+    var nowTime = +new Date();
+    if (lastTime && (lastTime + delay > nowTime)) {
+            setTimeout(function f(){}, 500);
+            return false;
+        } else {
+           localStorage.lastTime = nowTime;
+        }
     $(".menu-hidden").fadeIn("slow");
     if (every_second_click != 2) {
         every_second_click = 1;
