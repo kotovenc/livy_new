42339df655d4b585fef7bcb8ad774e3e247cbcf3
Koolana <kolya_andreychik@mail.ru>
2018-08-16 16:50:20 +0300

    Bubbles add; Stars modified;

--- js/LivYParticles.js
+++ js/LivYParticles.js
@@ -1,6 +1,12 @@
-var canvas;
+var canvasStars;
 var corrX = 0;
 var corrY = 0;
+
+var canvasBubbles;
+var oneBubble = [];
+var numBubble;
+var mouseX;
+var mouseY;
 
 function gaussianRand() {
   var rand = 0;
@@ -37,7 +43,7 @@ class Star{
   	draw(){
 		  this.ctx.fillStyle = this.color +', ' + Math.abs(this.alpha) + ')';
 		  this.ctx.beginPath(); 
-		  this.ctx.fillRect(this.x, this.y, 2, 2);
+		  this.ctx.fillRect(this.x, this.y, this.size, this.size);
 		  this.ctx.closePath(); 
 		  this.ctx.fill();
   	}
@@ -73,8 +79,8 @@ class Star{
   	born(){
   		var this_ = this;
 
-      this.x = getRandomInt(-corrX, canvas.width + corrX);
-      this.y = getRandomInt(-corrY, canvas.height + corrY);
+      this.x = getRandomInt(-corrX, canvasStars.width + corrX);
+      this.y = getRandomInt(-corrY, canvasStars.height + corrY);
 
   		this.timer = setTimeout(function tick() {
   			if(this_.flag == 1 && this_.alpha < this_.maxAlpha){
@@ -93,44 +99,176 @@ class Star{
   	}
 }
 
-$(window).resize(function(){
-  canvas.width = $('#particles').parent().width();
-  canvas.height = $('#particles').parent().height();
+class Bubble{
+    constructor(ctx, x, y) {
+      this.ctx = ctx;
+      this.x = x;
+      this.y = y;
+      this.vx = getRandomInt123(-1, 1);
+      this.vy = getRandomInt123(-1, 1);
 
-  corrX=parseInt(canvas.width/5);
-  corrY=parseInt(canvas.height/5);
+      this.maxAlpha = 0.5;
+      this.alpha = this.maxAlpha;
+
+      if(getRandomInt123(0, 1) == 0){
+        var randColor = getRandomInt123(0, 3)*30;
+        this.color = 'rgba('+ (200  - randColor) +', '+ (200 - randColor) +', '+ (0) +'';
+      }else{
+        var randColor = getRandomInt123(0, 1)*30;
+        this.color = 'rgba('+ (80) +', '+ (100) +', '+ (150 + randColor) +'';
+      }
+
+
+      this.size = getRandomInt123(10, 30);
+    }
+
+    draw(){
+      this.ctx.strokeStyle = this.color + ', ' + 0.5 + ')';
+      this.ctx.lineWidth=2;
+
+      this.ctx.fillStyle = this.color +', ' + Math.abs(this.alpha) + ')';
+
+      this.ctx.beginPath(); 
+      this.ctx.arc(this.x, this.y, this.size,50, 0, Math.PI*2, true);
+      this.ctx.arc(this.x, this.y, this.size-10,50, 0, Math.PI*2, true);
+      //this.ctx.fillRect(this.x, this.y, this.size, this.size);
+      this.ctx.closePath(); 
+      this.ctx.stroke();
+      this.ctx.fill();
+    }
+
+    died(){
+      var this_ = this;
+      this_.flag = 1;
+
+      this.timerDied = setTimeout(function tick() {
+        if(this_.alpha > 0.1 && this_.flag == 1){
+
+          this_.alpha -= 0.05;
+          this_.timerDied = setTimeout(tick, 50);
+        }else{
+          this_.flag = 0;
+        }
+      }, 50);
+    }
+
+    born(){
+      var this_ = this;
+      this.flag = 0;
+
+      this.timerBorn = setTimeout(function tick() {
+        if(this_.alpha < this_.maxAlpha && this_.flag == 0){
+          this_.alpha += 0.05;
+          this_.timerBorn = setTimeout(tick, 50);
+        }else{
+          this_.flag = 1;
+        }
+      }, 50);
+    }
+
+    move(){
+      if(this.x <= 0 || this.x >= canvasBubbles.width){
+        this.vx = -this.vx;
+      }
+
+      if(this.y <= 0 || this.y >= canvasBubbles.height){
+        this.vy = -this.vy;
+      }
+
+      this.x = this.x + this.vx;
+      this.y = this.y + this.vy;
+    }
+}
+
+$(window).resize(function(){
+  canvasStars.width = $('#stars').parent().width();
+  canvasStars.height = $('#stars').parent().height();
+
+  corrX=parseInt(canvasStars.width/5);
+  corrY=parseInt(canvasStars.height/5);
+
+  canvasBubbles.width = $('#bubbles').parent().width();
+  canvasBubbles.height = $('#bubbles').parent().height();
 });
 
 $(document).ready(function(){
-	canvas = document.getElementById("particles"),
-	ctx = canvas.getContext('2d');
-	canvas.width = $('#particles').parent().width();
-	canvas.height = $('#particles').parent().height();
+	canvasStars = document.getElementById("stars"),
+	ctx = canvasStars.getContext('2d');
+	canvasStars.width = $('#stars').parent().width();
+	canvasStars.height = $('#stars').parent().height();
 
-	corrX=parseInt(canvas.width/5);
-	corrY=parseInt(canvas.height/5);
+	corrX=parseInt(canvasStars.width/5);
+	corrY=parseInt(canvasStars.height/5);
 
-	startCanvas();
+  canvasBubbles = document.getElementById("bubbles"),
+  ctxB = canvasBubbles.getContext('2d');
+
+  $(document).mousemove(function(e){
+    var parentOffset = $('#bubbles').offset(); 
+    mouseX = e.pageX - parentOffset.left;
+    mouseY = e.pageY - parentOffset.top;
+  });
+
+  canvasBubbles.width = $('#bubbles').parent().width();
+  canvasBubbles.height = $('#bubbles').parent().height();
+
+  startCanvas();
 });
 
 function startCanvas(){
 	var oneStar = [];
-	var numStar = parseInt((canvas.width*canvas.height)/2500);
+	var numStar = parseInt((canvasStars.width*canvasStars.height)/2000);
 
 	for(var i = 0; i< numStar; i++){
-    oneStar[i] = new Star(ctx, getRandomInt(-corrX, canvas.width + corrX), getRandomInt(-corrY, canvas.height + corrY));
+    oneStar[i] = new Star(ctx, getRandomInt(-corrX, canvasStars.width + corrX), getRandomInt(-corrY, canvasStars.height + corrY));
 
 		oneStar[i].draw();
 
 		oneStar[i].life();
 	}
 
-	var timerId = setTimeout(function tick() {
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
+  numBubble = parseInt((canvasBubbles.width*canvasBubbles.height)/10000);
+
+  for(var i = 0; i< numBubble; i++){
+    oneBubble[i] = new Bubble(ctxB, getRandomInt123(0, canvasBubbles.width), getRandomInt123(0, canvasBubbles.height));
+
+    oneBubble[i].draw();
+
+    oneBubble[i].born();
+  }
+
+	var timerDrawStar = setTimeout(function tick() {
+		ctx.clearRect(0, 0, canvasStars.width, canvasStars.height);
 
 		for(var i = 0; i<numStar; i++){
 			oneStar[i].draw();
 		}
-  		timerId = setTimeout(tick, 50);
+  		timerDrawStar = setTimeout(tick, 50);
 	}, 50);
+
+  var timerDrawBubbles = setTimeout(function tick() {
+    ctxB.clearRect(0, 0, canvasBubbles.width, canvasBubbles.height);
+
+    for(var i = 0; i<numBubble; i++){
+      oneBubble[i].draw();
+    }
+
+    for(var i = 0; i<numBubble; i++){
+      oneBubble[i].move();
+    }
+
+    timerDrawBubbles = setTimeout(tick, 10);
+  }, 10);
+
+  var timerMouseDetectBubbles = setTimeout(function tick() {
+    for(var i = 0; i < numBubble; i++){
+      if(Math.pow(oneBubble[i].x - mouseX, 2) + Math.pow(oneBubble[i].y - mouseY, 2) <= 10000){
+        oneBubble[i].died();
+      }else{
+        oneBubble[i].born();
+      }
+    }
+
+    timerMouseDetectBubbles = setTimeout(tick, 20);
+  }, 20);
 }
