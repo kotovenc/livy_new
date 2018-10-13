4faf6a36a068864f78ac41182b7f88c845d044c7
Moonlight1337 <artoum555@gmail.com>
2018-10-13 10:37:57 +0300

    Бекап сайта

--- backend/form1.php
+++ backend/form1.php
@@ -1,34 +1,36 @@
 <?php
+	header('Content-Type: text/html; charset=utf-8');
 	if($_POST){
 		$name = htmlspecialchars($_POST["name"]);
 		$myemail = "artoum555@gmail.com";
-		if($_POST["mail"]){
-			$email = htmlspecialchars($_POST["mail"]);
+		if($_POST["email"]){
+			$email = htmlspecialchars($_POST["email"]);
 			$type = htmlspecialchars($_POST["type"]);
 			$msg = htmlspecialchars($_POST["text"]);
-			$message_to_myemail = "Дарова, епта<br>  
-			Типа имя - $name<br> 
-			Мыло уронил - $email<br> 
-			Это хчу - $type<br> 
-			А это мое мнение - msg<br> ";
+			$message_to_myemail = "Имя - $name\n 
+			Почта - $email \n 
+			Я хочу - $type \n 
+			Комментарий - $msg \n ";
 		}
 
 		if($_POST["phone"]){
 			$phone = htmlspecialchars($_POST["phone"]);
-			$message_to_myemail = "ЗдРаСтВуЙтЕ!!!!<br>
-			ХоЧу ПоГоВоРиТь О БоГе НаШеМ<br>
-			Я $name <br>
-			ЗвОнИ СюДа $phone<br>";
+			$message_to_myemail = "Имя - $name \n
+			Телефон - $phone \n";
 		}
 
 
 		$result = mail($myemail, $name, $message_to_myemail, $name);
 		if ($result){
-        	echo "Cообщение успешно отправленно. Пожалуйста, оставайтесь на связи<br>";
+        	echo "Cообщение успешно отправленно. Пожалуйста, оставайтесь на связи\n";
         	echo $message_to_myemail;
     	}
     	else {
-    		echo "Ti pidor> kekeke";
+    		echo "Что-то пошло не так";
     	}
+    }
+
+    else {
+    	echo "Vvedi dannie uebek";
     }
 ?>
